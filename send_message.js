09442f99e363a4a1088cd4c5fb0281310484f401
ivaninbox <ix.pukhov@gmail.com>
2024-02-08 19:35:41 +0600

    add new number

--- send_message.js
+++ send_message.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
-const send_message = async (products, totalCost, data, orderId) => {  // Добавьте параметр orderId
+
+const send_message = async (products, totalCost, data, orderId) => {
     const idInstance = '1101834631';
     const apiTokenInstance = 'f0aafa8020394baea4aa3db58aeb2afb02afca8b0e9b4ce4b5';
     const url = `https://api.green-api.com/waInstance${idInstance}/sendMessage/${apiTokenInstance}`;
@@ -10,7 +11,6 @@ ${product.name} - ${product.price} тенге x ${product.quantity} шт.
 ___
         `
     ).join('\n');
-
 
     const message = `Новый заказ:
 Товары:
@@ -28,20 +28,26 @@ ${data.phoneNumber}
     https://admin.miko-astana.kz/orders/${orderId}  
 `;
 
-    const payload = {
-        chatId: `77066001330@c.us`,
-        message: message
-    };
-
     const headers = {
         'Content-Type': 'application/json'
     };
 
-    try {
-        const response = await axios.post(url, payload, {headers});
-        console.log(response.data);
-    } catch (error) {
-        console.log(error);
+    // Фиксированные номера для отправки сообщений
+    const phoneNumbers = ['77066001330', '77020581667'];
+
+    // Цикл для отправки сообщения на каждый номер
+    for (let chatId of phoneNumbers) {
+        const payload = {
+            chatId: `${chatId}@c.us`,
+            message: message
+        };
+
+        try {
+            const response = await axios.post(url, payload, {headers});
+            console.log(`Сообщение отправлено на ${chatId}:`, response.data);
+        } catch (error) {
+            console.log(`Ошибка при отправке на ${chatId}:`, error);
+        }
     }
 };
 
