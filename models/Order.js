d36b448b954c7938760a4000b850512f14f89f27
ivaninbox <ix.pukhov@gmail.com>
2024-10-09 12:15:16 +0500

    f

--- models/Order.js
+++ models/Order.js
@@ -6,70 +6,82 @@ const moment = require('moment-timezone');
 
 const Order = {
 
-    async create(data, callback) {
-        const astanaTime = moment.tz("Asia/Almaty").format('YYYY-MM-DD HH:mm:ss');
 
-        // Получаем полную информацию о продуктах по их IDs
-        const productIds = data.products.map(product => product.id);
-        getByIds(productIds, async (error, products) => {
-            if (error) {
-                callback(error);
-                return;
-            }
 
-            // Вычисляем totalCost с учетом скидки для товаров, которые не относятся к категории "discont"
-            const totalCost = data.products.reduce((sum, product) => {
-                const fullProductInfo = products.find(p => p.id === product.id);
-                if (fullProductInfo.category === 'discont') {
-                    return sum + (fullProductInfo.price * product.quantity); // Не применяем скидку
+        async create(data, callback) {
+            const astanaTime = moment.tz("Asia/Almaty").format('YYYY-MM-DD HH:mm:ss');
+            console.log(`[INFO] Новый заказ создается. Время Астаны: ${astanaTime}`);
+
+            const productIds = data.products.map(product => product.id);
+            console.log(`[INFO] Идентификаторы продуктов: ${productIds}`);
+
+            getByIds(productIds, async (error, products) => {
+                if (error) {
+                    console.error(`[ERROR] Ошибка при получении продуктов: ${error.message}`);
+                    callback(error);
+                    return;
                 }
-                return sum + (fullProductInfo.price * product.quantity * 0.95); // Применяем скидку 5% к остальным товарам
-            }, 0);
 
-            // SQL-запрос для добавления нового заказа
-            const sql = `
+                const totalCost = data.products.reduce((sum, product) => {
+                    const fullProductInfo = products.find(p => p.id === product.id);
+                    if (fullProductInfo.category === 'discont') {
+                        return sum + (fullProductInfo.price * product.quantity);
+                    }
+                    return sum + (fullProductInfo.price * product.quantity * 0.95);
+                }, 0);
+
+                console.log(`[INFO] Общая стоимость заказа: ${totalCost}`);
+
+                const sql = `
 INSERT INTO orders 
 (firstName, lastName, phoneNumber, address, products, status, deliveryMethod, paymentMethod, totalCost, createdAt) 
 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 `;
 
-            db.run(sql,
-                [
-                    data.firstName,
-                    data.lastName,
-                    data.phoneNumber,
-                    data.address,
-                    JSON.stringify(data.products),
-                    data.status || "1",
-                    data.deliveryMethod,
-                    data.paymentMethod,
-                    totalCost,
-                    astanaTime
-                ],
-                async function (error) {
-                    if (error) {
-                        callback(error);
-                        return;
+                db.run(sql,
+                    [
+                        data.firstName,
+                        data.lastName,
+                        data.phoneNumber,
+                        data.address,
+                        JSON.stringify(data.products),
+                        data.status || "1",
+                        data.deliveryMethod,
+                        data.paymentMethod,
+                        totalCost,
+                        astanaTime
+                    ],
+                    async function (error) {
+                        if (error) {
+                            console.error(`[ERROR] Ошибка при добавлении заказа в базу данных: ${error.message}`);
+                            callback(error);
+                            return;
+                        }
+                        const orderId = this.lastID;
+                        console.log(`[INFO] Заказ успешно создан с ID: ${orderId}`);
+
+                        const productsWithQuantities = products.map((product, index) => {
+                            return {
+                                ...product,
+                                quantity: data.products[index].quantity
+                            };
+                        });
+
+                        console.log(`[INFO] Продукты с количеством: ${JSON.stringify(productsWithQuantities)}`);
+
+                        try {
+                            await send_message(productsWithQuantities, data, orderId);
+                            console.log(`[INFO] Сообщение об заказе отправлено успешно`);
+                        } catch (error) {
+                            console.error(`[ERROR] Ошибка при отправке сообщения: ${error.message}`);
+                        }
+
+                        callback(null, {id: orderId, totalCost});
                     }
-                    const orderId = this.lastID;  // Получаем ID нового заказа
+                );
+            });
+        },
 
-                    // Объединение информации о количестве с информацией о продуктах
-                    const productsWithQuantities = products.map((product, index) => {
-                        return {
-                            ...product,
-                            quantity: data.products[index].quantity
-                        };
-                    });
-
-                    // Отправляем сообщение с полной информацией о заказе
-                    await send_message(productsWithQuantities, totalCost, data, orderId);
-
-                    // Вызываем callback
-                    callback(null, {id: orderId, totalCost});
-                }
-            );
-        });
-    },
 
     delete(id, callback) {
         db.run('DELETE FROM orders WHERE id = ?', [id], callback);
